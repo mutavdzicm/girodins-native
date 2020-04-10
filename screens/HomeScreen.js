a754ebf150fb329516d6613ad0a54a3d0a8e34b2
Milos Mutavdzic <milos.mutavdzic@diwanee.com>
2020-04-10 16:56:30 +0200

    First commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import { MonoText } from '../components/StyledText';
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+        <MonoText>First component</MonoText>
+      </ScrollView>
+
+    </View>
+  );
+}
+
+HomeScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+
+});
